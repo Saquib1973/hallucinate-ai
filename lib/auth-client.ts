f23b02f83b32b870e71747114dbdf751e4844ec9
Saquib Ali <saquibali353@gmail.com>
2026-02-21 08:19:01 +0530

    production fix

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 export const { signIn, signUp, signOut, useSession } = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://hallucinate-ai.vercel.app"
 })
